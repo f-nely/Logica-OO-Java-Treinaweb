d05b8a4079edc05258d976c6acc306c50b05f01e
nely de faria <nelly.nf54@gmail.com>
2020-07-26 18:05:48 -0300

    Criação do construtor da classe Animal

--- Zoologico/src/com/nelynely/zoologico/classes/Animal.java
+++ Zoologico/src/com/nelynely/zoologico/classes/Animal.java
@@ -1,37 +1,47 @@
 package com.nelynely.zoologico.classes;
 
 public class Animal {
-	
+
 	private String name;
 	private String species;
 	private int age;
-	
+
+	public Animal(String name, String species, int age) {
+		this.name = name;
+		this.species = species;
+		this.age = age;
+	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getSpecies() {
 		return species;
 	}
+
 	public void setSpecies(String species) {
 		this.species = species;
 	}
+
 	public int getAge() {
 		return age;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
-	
-	
+
 	public void emitirBarulho() {
 		System.out.println("Barulho do animal: ");
 	}
-	
+
 	public Boolean ehAdulto() {
-		
+
 		return age >= 1;
 	}
 
