3d16a1f0c4be2e0dee5c7a291b94375bdc17ae32
nely de faria <nelly.nf54@gmail.com>
2020-07-26 17:15:28 -0300

    Adiciona classe Animal

---
+++ Zoologico/src/com/nelynely/zoologico/classes/Animal.java
@@ -0,0 +1,32 @@
+package com.nelynely.zoologico.classes;
+
+public class Animal {
+	
+	private String name;
+	private String species;
+	private int age;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSpecies() {
+		return species;
+	}
+	public void setSpecies(String species) {
+		this.species = species;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	
+	
+
+}
