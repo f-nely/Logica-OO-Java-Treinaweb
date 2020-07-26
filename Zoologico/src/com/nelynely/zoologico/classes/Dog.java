013965301d131ab6769988b8985a1877d5e8d8e1
nely de faria <nelly.nf54@gmail.com>
2020-07-26 19:08:30 -0300

    Adiciona classe Dog

---
+++ Zoologico/src/com/nelynely/zoologico/classes/Dog.java
@@ -0,0 +1,18 @@
+package com.nelynely.zoologico.classes;
+
+public class Dog extends Animal{
+
+	public Dog(String name) {
+		super(name);
+		
+	}
+	
+	public Dog(String name, String species, int age) {
+		super(name, species, age);
+	}
+	
+	public Dog(String name, int age) {
+		super(name, "dog", age);
+	}
+
+}
